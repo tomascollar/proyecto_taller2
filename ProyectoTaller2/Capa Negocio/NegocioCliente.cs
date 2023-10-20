ba7115a2f805b332361f64181b2d678e93402c8b
tomascollar <tomascollar@gmail.com>
2023-10-20 03:57:36 -0300

    .

--- ProyectoTaller2/Capa Negocio/NegocioCliente.cs
+++ ProyectoTaller2/Capa Negocio/NegocioCliente.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ProyectoTaller2.Capa_Datos;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,30 @@ namespace ProyectoTaller2.Capa_Negocio
 {
     internal class NegocioCliente
     {
+
+        private DatosCliente datos = new DatosCliente();
+
+        public bool AgregarCliente(string nombre, string apellido, int dni, string telefono, string direccion)
+        {
+            var obj = new clientes
+            {
+                nombre_cliente = nombre,
+                apellido_cliente = apellido,
+                DNI_cliente = dni,
+                telefono_cliente = telefono,
+                direccion_cliente = direccion
+            };
+
+            datos.InsertarCliente(obj);
+
+            return true;
+        }
+
+        public List<clientes> ListarClientes()
+        {
+            var lst = datos.ObtenerClientes();
+            return lst;
+        }
+
     }
 }
