703fee9db57eb0125bc66d7329c26520b2e8279c
tomascollar <tomascollar@gmail.com>
2023-10-20 03:42:21 -0300

    Agregado clases de la capa de datos para todas las tablas

---
+++ ProyectoTaller2/Capa Datos/DatosProducto.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoTaller2.Capa_Datos
+{
+    internal class DatosProducto
+    {
+        public void InsertarProducto(productos nuevoProducto)
+        {
+            using(var context = new proyecto_taller2Entities())
+            {
+                context.productos.Add(nuevoProducto);
+                context.SaveChanges();
+            }
+        }
+
+        public List<productos> ObtenerProductos()
+        {
+            using (var context = new proyecto_taller2Entities())
+            {
+                return context.productos.ToList();
+            }
+        }
+    }
+}
